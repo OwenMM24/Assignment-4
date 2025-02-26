a9b2269482dc80a1759ddf91be4ef2cf49dd2f08
OwenMM24 <ommiller661@gmail.com>
2025-02-26 16:56:47 -0500

    quick push

---
+++ Assets/Scripts/EquipItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipItem : MonoBehaviour
+{
+
+    [SerializeField] GameObject placeObject;
+    [SerializeField] GameObject objectType;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
