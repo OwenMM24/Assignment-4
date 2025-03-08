65a902730488bb9baade60c7869eaeb4285fc15d
ago12627 <audrey12627@gmail.com>
2025-03-08 14:40:19 -0500

    adjust prefabs

--- Assets/Scripts/OutfitManager.cs
+++ Assets/Scripts/OutfitManager.cs
@@ -4,23 +4,29 @@ using Vuforia;
 
 public class OutfitManager : MonoBehaviour
 {
-    public GameObject model;  // Base model to overlay clothes on
-    public Transform overlayParent; // Parent object where clothes are added
-    public int maxLayers = 6;       // Limit max clothing overlays
+    public GameObject model;
+    public Transform overlayParent;
+    public int maxLayers = 6;
     private List<GameObject> activeClothes = new List<GameObject>();
 
     public void AddClothing(GameObject clothingPrefab)
     {
         if (activeClothes.Count >= maxLayers)
         {
-            // Remove oldest clothing item if limit is exceeded
             Destroy(activeClothes[0]);
             activeClothes.RemoveAt(0);
         }
 
-        // Instantiate new clothing item and attach it to the person
         GameObject newClothing = Instantiate(clothingPrefab, overlayParent);
         activeClothes.Add(newClothing);
+    }
+
+    public void RemoveClothing(GameObject clothingInstance)
+    {
+        if (clothingInstance != null)
+        {
+            Destroy(clothingInstance);
+        }
     }
 
     public void ClearOutfit()
