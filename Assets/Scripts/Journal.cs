dc9949989c54544820536b126fd581017f5bc0e0
OwenMM24 <ommiller661@gmail.com>
2025-03-11 22:59:42 -0400

    added different ui elements and patched journal a bit more

--- Assets/Scripts/Journal.cs
+++ Assets/Scripts/Journal.cs
@@ -12,23 +12,30 @@ using TMPro;
 public class Journal : MonoBehaviour
 {
     public GameObject[] clothes = new GameObject[5];
+    public int[] clothesUses = new int[5];
     int currentClothesIndex = 0;
     [SerializeField] Transform obj;
     [SerializeField] GameObject multiParent;
-    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] TextMeshProUGUI slotText;
+    [SerializeField] TextMeshProUGUI usesText;
+    int outfitUses = 0;
 
     public void AddSet()
     {
         GameObject set = Instantiate(obj.gameObject);
         set.SetActive(false);
+        if (clothes[currentClothesIndex] != null )
+        {
+            Destroy(clothes[currentClothesIndex]);
+        }
         clothes[currentClothesIndex] = set;
 
         DeleteChildren();
 
-        //clothes[currentClothesIndex].SetActive();
+        
         clothes[currentClothesIndex].transform.SetParent(multiParent.transform);
         clothes[currentClothesIndex].transform.localPosition = new Vector3(0, 0, 0);
-        //Debug.LogWarning(set.transform.position);
+        
         clothes[currentClothesIndex].transform.localRotation = Quaternion.Euler(0, 0, 0);
         currentClothesIndex = (currentClothesIndex + 1) % clothes.Length;
 
@@ -53,7 +60,8 @@ public class Journal : MonoBehaviour
             clothes[currentClothesIndex].SetActive(false);
 
         currentClothesIndex = (currentClothesIndex - 1 + clothes.Length) % clothes.Length;
-        text.text = $"[Slot: {currentClothesIndex}]";
+        slotText.text = $"[Slot: {currentClothesIndex}]";
+        usesText.text = $"Outfit Worn {clothesUses[currentClothesIndex]} Times";
 
         if (clothes[currentClothesIndex] != null)
             clothes[currentClothesIndex].SetActive(true);
@@ -65,25 +73,19 @@ public class Journal : MonoBehaviour
             clothes[currentClothesIndex].SetActive(false);
 
         currentClothesIndex = (currentClothesIndex + 1) % clothes.Length;
-        text.text = $"[Slot: {currentClothesIndex}]";
+        slotText.text = $"[Slot: {currentClothesIndex}]";
+        usesText.text = $"Outfit Worn {clothesUses[currentClothesIndex]} Times";
 
         if (clothes[currentClothesIndex] != null)
             clothes[currentClothesIndex ].SetActive(true);
     }
 
-
-    /*void AddSet(GameObject[] set)
+    public void IncreaseOutfitUses()
     {
-        if (currentClothesCount + set.Length <= clothes.Length)
-        {
-            for (int i = 0; i < set.Length; i++)
-            {
-                clothes[currentClothesCount] = set[i];
-                currentClothesCount++;
-            }
-        }
+        clothesUses[currentClothesIndex]++;
+        usesText.text = $"Outfit Worn {clothesUses[currentClothesIndex]} Times";
+        //outfitUses++;
 
-    }*/
-
+    }
 
 }
